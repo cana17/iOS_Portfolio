288c331fd8d5bad2c59f41eaf082b20d8ca3d4d8
Cana <94155642+woojinchoi17@users.noreply.github.com>
2023-05-30 22:20:23 +0900

    에어센스

---
+++ AirSense/AirSense/AlarmViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AlarmViewController.swift
+//  AirSense
+//
+//  Created by woojin Choi on 2023/05/30.
+//
+
+import UIKit
+
+class AlarmViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

0d402b3911bed03aecaeab09e505bf19afe5384b
woojinchoi17 <cwj0717@naver.com>
2023-06-06 20:53:57 +0900

    Add RLC Calulator

---
+++ RLC Calculator/RLC Calculator/CapacitorViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CapacitorViewController.swift
+//  RLC Calculator
+//
+//  Created by woojin Choi on 2023/06/06.
+//
+
+import UIKit
+
+class CapacitorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ RLC Calculator/RLC Calculator/ResisterViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ResisterViewController.swift
+//  RLC Calculator
+//
+//  Created by woojin Choi on 2023/06/06.
+//
+
+import UIKit
+
+class ResisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
